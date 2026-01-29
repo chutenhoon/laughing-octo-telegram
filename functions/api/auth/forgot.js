18011c883c30944c510c9ae90f99b370e65248a6
lythanhtoan8298 <lythanhtoan8298@sevencolors.online>
2026-01-29 18:28:14 +0700

    delete all files

--- functions/api/auth/forgot.js
+++
@@ -1,6 +0,0 @@
-export async function onRequestPost({ request, env }) {
-  return new Response(JSON.stringify({ ok: true }), {
-    status: 200,
-    headers: { "content-type": "application/json" },
-  });
-}
--- functions/api/following.js
+++
@@ -1,8 +0,0 @@
-import { onRequestGet as followGet } from "./follow.js";
-
-export async function onRequestGet(context) {
-  const url = new URL(context.request.url);
-  url.searchParams.set("list", "following");
-  const request = new Request(url.toString(), context.request);
-  return followGet({ ...context, request });
-}
